f44258d855a246eb99b0afbc9d0f1f18e3a3f64f
Wenhan Zhang <wenhanzhang@WenhandeMacBook-Pro.local>
2019-10-01 12:10:41 -0700

    make it better

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const mongodb = require('mongodb').MongoClient;
+
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true
 });
-
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .get('/times', (req, res) => res.send(showTimes()))
     .get('/db', async (req, res) => {
         try {
             const client = await pool.connect()
@@ -29,7 +26,9 @@ express()
             res.send("Error " + err);
         }
     })
-
+    .get('/cool', (req, res) => res.send(cool()))
+    .get('/times', (req, res) => res.send(showTimes()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
     let result = ''
